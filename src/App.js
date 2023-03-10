37223f7c89fdb879d21ba9d3ebaa32739c8d7d4d
Rizian <91533618+Rizian@users.noreply.github.com>
2023-03-10 23:48:32 +0700

    Update main file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import './styles/App.css';
+
+import AuthorInfo from './components/AuthorInfo';
+import PageTitle from './components/PageTitle';
+// import TodoAdd from './components/TodoAdd';
+// import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-container">
+      <AuthorInfo text="Made by: Ian Wirawan"></AuthorInfo>
+      <AuthorInfo text="Student ID: 2502009596"></AuthorInfo>
+      <PageTitle text="TODO List"></PageTitle>
+      <div className="Todo-list-container">
+        
+      </div>
     </div>
   );
 }
